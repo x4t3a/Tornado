8dd8040f86ce296be1475d6bf7abb46b69eff445
Andrey Dunaev <0h4t3a@gmail.com>
2020-08-30 22:11:42 +0300

    Added react-native/navigate

--- App.js
+++ App.js
@@ -1,93 +1,99 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
+ * 🌪 Tornado 🌪
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+    ActivityIndicator,
+    FlatList,
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function FeedScreen() {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    
+//    const api_token = "08de80d6e74322e9e0cbcfec3d53bf8d9b49269a4b45e8ff3b24c365f7cc7e4e1ddcb65819e601ae3f414";
+//    const api_version = "5.122";
+//
+//    useEffect(() => {
+//        fetch("https://api.vk.com/method/newsfeed.get?access_token=08de80d6e74322e9e0cbcfec3d53bf8d9b49269a4b45e8ff3b24c365f7cc7e4e1ddcb65819e601ae3f414&v=5.122")
+//            .then((response) => response.json())
+//            .then((json) => setData(json))
+//            .catch((error) => console.error(error))
+//            .finally(() => setLoading(false));
+//    }, []);
+  
+  useEffect(() => {
+    fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => setData(json.movies))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+    return (
+//        <View style={{ flex: 1, padding: 24 }}>
+//          {isLoading ? <ActivityIndicator/> : (
+//            <FlatList
+//              data={data}
+//              renderItem={({ item }) => (
+//                <Text>{item}</Text>
+//              )}
+//            />
+//          )}
+//        </View>
+<View style={{ flex: 1, padding: 24 }}>
+  {isLoading ? <ActivityIndicator/> : (
+    <FlatList
+      data={data}
+      keyExtractor={({ id }, index) => id}
+      renderItem={({ item }) => (
+        <Text>{item.title}, {item.releaseYear}</Text>
+      )}
+    />
+  )}
+</View>
+
+    );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Darova, Ych</Text>
-              <Text style={styles.sectionDescription}>
-                Darova, <Text style={styles.highlight}>Ych!</Text>
-              </Text>
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Feed" component={FeedScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
--- metro.config.js
+++ metro.config.js
@@ -14,4 +14,7 @@ module.exports = {
       },
     }),
   },
+  resolver: {
+   sourceExts: ['jsx','js', 'ts', 'tsx'] // add tsx if its not yet defined
+  },
 };
